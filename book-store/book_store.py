63931d581734c005d2b6ccc2e18ebec0042f4e93
Andy Marks <vampwillow@gmail.com>
2025-07-28 12:06:17 +1000

    Overarching gitignore for python

--- book-store/book_store.py
+++ book-store/book_store.py
@@ -1,25 +1,27 @@
+from collections import Counter
+
 book_price = 800
 
 def total(basket):
-    unique_books = len(set(basket))
-    number_of_books_purchased = len(basket)
+    distribution = Counter(basket)
+    unique_books = len(distribution)
 
-    price = 0
-    if unique_books == 1:
-        price = book_price
-    if unique_books == 2:
-        price = discount_for_quantity(unique_books, 5)
-    if unique_books == 3:
-        price = discount_for_quantity(unique_books, 10)
-    if unique_books == 4:
-        price = discount_for_quantity(unique_books, 20)
-    if unique_books == 5:
-        price = discount_for_quantity(unique_books, 25)
+    discounts = {0: 0, 1: 0, 2: 5, 3: 10, 4: 20, 5: 25}
     
+    price = 0
+    price = apply_discount(unique_books, discounts[unique_books])
+    
+    print(f"current price: {price}")
+    number_of_books_purchased = len(basket)
     if (number_of_books_purchased > unique_books):
+
+        print(f"overflow of {number_of_books_purchased - unique_books} books")
         price += (number_of_books_purchased - unique_books) * book_price
+
+    print(f"final price: {price}")
 
     return price
 
-def discount_for_quantity(quantity, discount):
+def apply_discount(quantity: int, discount: int) -> int:
+    print(f"applying discount of {discount} to {quantity} books")
     return book_price * quantity - ((book_price * quantity) * discount / 100)
